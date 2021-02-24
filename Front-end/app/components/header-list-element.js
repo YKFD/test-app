775186b439d5ac4e63afdb762a24668b1ba11362
YKFD <chervonchuk316@gmail.com>
2021-02-25 00:02:45 +0200

    routers were added

---
+++ Front-end/app/components/header-list-element.js
@@ -0,0 +1,12 @@
+import Component from '@glimmer/component';
+import {tracked} from '@glimmer/tracking';
+
+export default class AppHeader extends Component {
+  @tracked categoryUrl;
+
+  constructor(owner, args) {
+    super(owner, args);
+    this.categoryUrl = args.category.name.toLowerCase();
+    console.log(args.category.name.toLowerCase())
+  }
+}
--- Front-end/app/router.js
+++ Front-end/app/router.js
@@ -6,4 +6,9 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('News');
+  this.route('news');
+  this.route('forum');
+  this.route('blogs');
+});
